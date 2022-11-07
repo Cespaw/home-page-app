cca711e7a060a61b7f08f5d4d508ff796fa2d46e
Cespaw <cesarp@hotmail.se>
2022-11-07 10:29:11 +0100

    Added functionality to update-function

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,28 +5,49 @@ function TodoItem({ props, handleDelete, handleEdit }) {
     const [editClicked, setEditClicked] = useState(false)
     const [input, setInput] = useState("")
 
-    function handleEditClicked(){
+    function handleEditClicked() {
+        setEditClicked(!editClicked)
+        setInput(props.name)
+    }
+
+    function handleConfirmClicked() {
+        handleEdit(props.id, input)
         setEditClicked(!editClicked)
     }
 
     return (
         <div className="ListItem">
             <div>
-                {props.name}
-                <button type="button" onClick={() => handleEditClicked()}>Edit</button>
-
-                <button type="button" onClick={() => handleDelete(props.id)}>Delete</button>
-
-                {editClicked ? 
-                <div>
-                        <input value={input} 
-                               onChange={(event) => setInput(event.target.value)}>
+                {editClicked ?
+                    <>
+                        <input 
+                            value={input}
+                            placeholder={props.name}
+                            onChange={(event) => setInput(event.target.value)}>
                         </input>
-                        <button type="button" onClick={(e) => handleEdit(props.id, input)}>Confirm</button>
-                        
-                </div>
-                : <></>}
 
+                        <button 
+                            type="button" 
+                            onClick={(e) => handleConfirmClicked(props.id, input)}>
+                            Confirm
+                        </button>
+                    </>
+                    : 
+                    <>
+                        {props.name}
+                        <button 
+                            type="button" 
+                            onClick={() => handleEditClicked()}>
+                            Edit
+                        </button>
+                    </>
+                }
+
+                <button 
+                    type="button" 
+                    onClick={() => handleDelete(props.id)}>
+                    Delete
+                </button>
             </div>
         </div>
     );
